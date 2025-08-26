122e677c5cc6af5b48b6c23470f8cab39391eeac
NickPrather <nprather@students.mchenry.edu>
2025-08-26 11:23:07 -0500

    PythonDataTypes

---
+++ pythondatatypes.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+y = -20
+print("Integer:", y)
+
+# Float
+z = 12.67
+print("Float:", z)
+
+# String
+name = "John pork"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
