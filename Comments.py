d0202329630f8cd8f7951e422b135b774e9f0dec
NickPrather <nprather@students.mchenry.edu>
2025-09-04 11:02:35 -0500

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,16 @@
+"""
+
+Type the above code into Visual Studio Code
+Add a minimum of 4 comments to the above Python program to make it more understandable.
+"""
+# Define the size of the tank
+tank_size = 15
+
+# Define the total distance
+distance = 600
+
+# Calculate miles per gallon of the tank
+mpg = distance / tank_size
+
+# Print the miles per gallon
+print("Miles per gallon:", mpg)
